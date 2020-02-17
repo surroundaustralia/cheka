abbd7018cdb16900355da60e4941c00f82ca23f9
Nicholas Car <nicholas.car@csiro.au>
2020-02-17 12:07:34 +1000

    comments for tests

--- src/tests/test_01.py
+++ src/tests/test_01.py
@@ -1,5 +1,9 @@
 from src.cheka import Cheka
 
+"""
+These most basic tests ensure that a single thing in a data graph can be validated according to a profile hierarchy for 
+a given Profile (by URI). 
+"""
 c = Cheka('test_01_d.ttl', 'test_01_p.ttl')
 
 assert c.validate(profile_uri='http://example.org/profile/Profile_B')[0]
--- src/tests/test_02.py
+++ src/tests/test_02.py
@@ -1,5 +1,15 @@
 from src.cheka import Cheka
 
+"""
+These tests check that the objects in the data graph can be validated only according to the Profiles they claim 
+conformance to. In test_02_d.ttl, they are:
+
+<http://example.org/dataset/One> dct:conformsTo <http://example.org/profile/Profile_C> ;
+
+<http://example.org/dataset/Two> dct:conformsTo <http://example.org/profile/Profile_B> ;
+
+This test is designed to ensure that not every single thing in the data graph is validated according to very Profile.
+"""
 c = Cheka('test_02_d.ttl', 'test_01_p.ttl')
 
 a = c.validate()
